bc7957288b0f9517746248b25a5ebeab36ff35e7
srinivasa reddy muly <msrinivasareddy@gmail.com>
2016-06-30 15:35:47 -0400

    added types for acttvity, goal, activity log

--- types/activity.go
+++ types/activity.go
@@ -1,12 +1,25 @@
 package types
 
 import (
-//"time"
+	"time"
 )
 
-type Activity struct {
-	Name      string
-	SubTask   string
-	StartTime string
-	EndTime   string
-}
+type (
+	Activity struct {
+		Name      string
+		GoalID    string
+		StartTime string
+		EndTime   string
+	}
+
+	Goal struct {
+		Name  string
+		Notes string
+	}
+
+	AcctivityLog struct {
+		ActivityName string
+		StartTime    time.Time
+		EndTime      time.Time
+	}
+)
